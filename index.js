24777384d68fa87af9969b22c6d2091cdece1668
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-14 18:33:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -100,10 +100,11 @@ function removeFromCart(item) {
     }
   } */
   
-  if(!cart.includes(item)){
+  if(!cart.includes(item)){   // this passes fine
     return "That item is not in your cart."
   }
   
+  else
   for(let i = 0; i < cart.length; i++)
   {
     if(cart[i] != item)
